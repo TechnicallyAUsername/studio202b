6f09b857749f8b7578591ff24ba0d9238ca99847
Tanya Goncalves <goncat@mcmaster.ca>
2014-11-29 22:07:56 -0500

    Changes: Playing sounds/music with keyboard functionality. Author: Tanya Goncalves (goncat@mcmaster.ca).

--- Master Files/globalVariables/audio.py
+++ Master Files/globalVariables/audio.py
@@ -15,17 +15,17 @@ audio.drumloop3 = aud.Factory.file(path+"/5-loops/drum loop 128 bpm 3.wav")
 audio.drumloop4 = aud.Factory.file(path+"/5-loops/drum loop 125 bpm 2.wav")
 audio.drumloop5 = aud.Factory.file(path+"/5-loops/drum loop 125 bpm 3.wav")
 
-# 8 music sample sets by Matt hyland
-audio.snareSolo = aud.Factory.file(path+"/8-samples/snape solo.wav")
-audio.snarehalfbar = aud.Factory.file(path+"/8-samples/snare half bar.wav")
-audio.openHatSolo= aud.Factory.file(path+"/8-samples/open hat solo.wav")
-audio.openHatHalfBar= aud.Factory.file(path+"/8-samples/open hat half bar.wav")
-audio.kickSolo = aud.Factory.file(path+"/8-samples/kick solo.wav")
-audio.dunno = aud.Factory.file(path+"/8-samples/dunno.wav")
-audio.drumsWithLoops = aud.Factory.file(path+"/8-samples/drums with loops.wav")
-audio.drumsTogether = aud.Factory.file(path+"/8-samples/drums together.wav")
-audio.closedHighHatSolo = aud.Factory.file(path+"/8-samples/closed high hat solo.wav")
-audio.closedHighHatHalfBar = aud.Factory.file(path+"/8-samples/closed high hat half bar.wav")
+# 10 music sample sets by Matt hyland
+audio.snareSolo = aud.Factory.file(path+"/8-samples/snare solo.mp3")
+audio.snarehalfbar = aud.Factory.file(path+"/8-samples/snare half bar.mp3")
+audio.openHatSolo= aud.Factory.file(path+"/8-samples/open hat solo.mp3")
+audio.openHatHalfBar= aud.Factory.file(path+"/8-samples/open hat half bar.mp3")
+audio.kickSolo = aud.Factory.file(path+"/8-samples/kick solo.mp3")
+audio.dunno = aud.Factory.file(path+"/8-samples/dunno.mp3")
+audio.drumsWithLoops = aud.Factory.file(path+"/8-samples/drums with loops.mp3")
+audio.drumsTogether = aud.Factory.file(path+"/8-samples/drums together.mp3")
+audio.closedHighHatSolo = aud.Factory.file(path+"/8-samples/closed high hat solo.mp3")
+audio.closedHighHatHalfBar = aud.Factory.file(path+"/8-samples/closed high hat half bar.mp3")
 
 
 
